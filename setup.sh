65ca9a28d76e79abf27d87c7cfd576eb1ea35caa
daniblu <mr.daniblu@gmail.com>
2023-05-03 11:51:48 +0200

    setting up repo

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# create virtual environment
+python3 -m venv assignment4_env
+
+# activate virtual environment
+source ./assignment4_env/bin/activate
+
+# update pip, install requirements
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
